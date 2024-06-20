3967947153c5cb8d14d7ad9f3372dba9e098648e
kwasinsiah <truegrayowl@gmail.com>
2024-06-20 18:40:35 -0400

    started new app

---
+++ Apple-frameworks/Apple-frameworks/Apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_frameworksApp.swift
+//  Apple-frameworks
+//
+//  Created by Kwasi Nsiah on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
